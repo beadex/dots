405a92828f5fd1a01570c19175e33d859a22aa17
Phong Tran <tranphongbb@icloud.com>
2020-10-21 01:25:52 +0700

    remove bottom bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,4 +10,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar top -c ~/.config/polybar/config-top.ini &
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
